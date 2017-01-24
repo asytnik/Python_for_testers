1b13365023338d003ccd0c623fe2d9356e871e6f
asytnik <asytnik07@gmail.com>
2017-01-24 18:03:08 -0500

    Add comments to script

--- new_contact.py
+++ new_contact.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -16,9 +15,10 @@ class new_contact(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_new_contact(self):
-        success = True
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/")
+        # login into
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -26,7 +26,9 @@ class new_contact(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        # init contact creation
         wd.find_element_by_link_text("add new").click()
+        # fill name_occupation_address
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("ivan")
@@ -45,6 +47,7 @@ class new_contact(unittest.TestCase):
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
         wd.find_element_by_name("address").send_keys("1230 Avenue X, Brooklyn, NY")
+        # fill phone number
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
         wd.find_element_by_name("home").send_keys("+1 113 456 987 654")
@@ -55,6 +58,7 @@ class new_contact(unittest.TestCase):
         wd.find_element_by_name("work").clear()
         wd.find_element_by_name("work").send_keys("+2 325 685 965 784")
         wd.find_element_by_name("fax").click()
+        # fill e-mail_DOB
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys("balda@mail.ru")
@@ -68,14 +72,16 @@ class new_contact(unittest.TestCase):
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1990")
+        # fill second address
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
         wd.find_element_by_name("address2").send_keys("4554 Neptune Av., Brooklyn,NY")
+        # enter_credentials
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        # return to hp_logout
         wd.find_element_by_link_text("home").click()
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
     def tearDown(self):
         self.wd.quit()
 
