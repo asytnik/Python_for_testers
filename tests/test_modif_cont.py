882f8ce31cb86522eef84d625bce2b287eca24d1
asytnik <asytnik07@gmail.com>
2017-02-14 19:05:28 -0500

    Modification contacts and groups
    Corrected second time

--- tests/test_modif_cont.py
+++ tests/test_modif_cont.py
@@ -2,8 +2,8 @@
 from model.param import Param
 
 def test_modif_cont(apl):
-    if apl.contact.contact_counter() > 0:
-        apl.contact.del_first_contact()
-    apl.contact.fill_credentials(Param(firstname="new ivan", lastname="new ivanov", nickname="sorry", title="crecker", company="BBB",
+    if apl.contact.contact_counter() == 0:
+        apl.contact.fill_credentials(Param(firstname="new ivan", lastname="new ivanov", nickname="sorry", title="crecker", company="BBB",
                   address="without"))
+    apl.contact.del_first_contact()
 
--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -2,8 +2,8 @@
 from model.group import Group
 
 def test_modify_fill_group_name(apl):
-    if apl.group.counter_group() > 0:
-        apl.group.delete_first_group()
-    apl.group.edit_first_group(Group(name="ggigigiu"))
+    if apl.group.counter_group() == 0:
+        apl.group.edit_first_group(Group(name="ggigigiu"))
+    apl.group.delete_first_group()
 
 
