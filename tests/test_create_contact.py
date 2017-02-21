ba92b103a816b47ec7185c127a78a96b290f302a
asytnik <asytnik07@gmail.com>
2017-02-21 09:52:57 -0500

    Compare lists with asserts
    Full edition, working variant

--- tests/test_create_contact.py
+++ tests/test_create_contact.py
@@ -13,10 +13,15 @@ def test_create_contact(apl):
 
 
 
-#def test_with_empty_spaces(apl):
-#    old_contacts = apl.contact.get_contact_list()
-#    apl.contact.empty_spaces(Param(firstname="empty1", lastname="empty2"))
-#    new_contacts = apl.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
+def test_with_empty_spaces(apl):
+    old_contacts = apl.contact.get_contact_list()
+    contact = Param(firstname="", lastname="")
+    apl.contact.empty_spaces(contact)
+    new_contacts = apl.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Param.max_or_id) == sorted(new_contacts, key=Param.max_or_id)
+
+
 
 
--- tests/test_modif_cont.py
+++ tests/test_modif_cont.py
@@ -3,12 +3,12 @@ from model.param import Param
 
 def test_modif_cont(apl):
     if apl.contact.contact_counter() == 0:
-        apl.contact.new_contact_creation(Param(firstname="old ivan", lastname="old ivanov", nickname="buddy", title="hooker", company="AAA",
-                  address="with address"))
+        apl.contact.new_contact_creation(Param(firstname="old ivan", lastname="old ivanov"))
     old_contacts = apl.contact.get_contact_list()
-    apl.contact.edit_first_contact(Param(firstname="new ivan", lastname="new ivanov", nickname="sorry", title="crecker", company="BBB",
-                  address="without address"))
+    contacts = Param(firstname="mod ivan", lastname="mod ivanov")
+    contacts.id = old_contacts[0].id
+    apl.contact.edit_first_contact(contacts)
     new_contacts = apl.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-
-
+    old_contacts[0] = contacts
+    assert sorted(old_contacts, key=Param.max_or_id) == sorted(new_contacts, key=Param.max_or_id)
