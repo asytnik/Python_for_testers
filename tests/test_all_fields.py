59f94813e1fb8a96965c8dffd55ffd968f3d6540
asytnik <asytnik07@gmail.com>
2017-02-28 12:24:04 -0500

    Separate test file for all assertions
    create separate test_file for all assertions in bundle

---
+++ tests/test_all_fields.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import re
+from random import randrange
+
+def test_all_fields_on_home_page(apl):
+    home_page_contacts = apl.contact.get_contact_list()
+    index = randrange(len(home_page_contacts))
+    info_from_home_page = apl.contact.get_contact_list()[index]
+    info_from_edit_page = apl.contact.get_contact_info_from_edit_page(index)
+    assert info_from_home_page.lastname == info_from_edit_page.lastname
+    assert info_from_home_page.firstname == info_from_edit_page.firstname
+    assert info_from_home_page.address == info_from_edit_page.address
+    assert info_from_home_page.all_email_from_home_page == merge_email_like_home_page(info_from_edit_page)
+    assert info_from_home_page.all_phones_from_home_page == merge_phones_like_home_page(info_from_edit_page)
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_email_like_home_page(param):
+    return "\n".join([param.email, param.email2, param.email3])
+
+def merge_phones_like_home_page(param):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                   [param.homephone, param.mobilephone, param.workphone]))))
--- tests/test_email.py
+++ tests/test_email.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-
 import re
+from random import randrange
 
 def test_email_on_home_page(apl):
-    contact_from_home_page = apl.contact.get_contact_list()[0]
-    contact_from_edit_page = apl.contact.get_contact_info_from_edit_page(0)
+    home_page_contacts = apl.contact.get_contact_list()
+    index = randrange(len(home_page_contacts))
+    contact_from_home_page = apl.contact.get_contact_list()[index]
+    contact_from_edit_page = apl.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_email_from_home_page == merge_email_like_home_page(contact_from_edit_page)
 
 def clear(s):
