6bf8bb1113586c29ab3710937adf7dafe357bbc2
asytnik <asytnik07@gmail.com>
2017-03-21 22:40:24 -0400

    Test_modify_group_by_id

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -10,7 +10,7 @@ def test_modify_group_by_index(apl, db):
         apl.group.creation_group(Group(name="New group", header="Old header", footer="Old footer"))
     old_groups = apl.group.get_group_info()
     index = randrange(len(old_groups))
-    group = Group(name="Old group", header="Old header", footer="Old footer")
+    group = Group(name="Just group", header="Just header", footer="Just footer")
     group.id = old_groups[index].id
     apl.group.edit_group_by_index(index, group)
     new_groups = db.get_group_info()
@@ -30,19 +30,23 @@ def test_modify_group_by_id(apl, db, check_ui):
         apl.group.creation_group(Group(name="New group", header="New header", footer="New footer"))
     old_groups = db.get_group_info()
     group = random.choice(old_groups)
-    new_group_data = Group(name="Bingo_lotto", header="Strike_header", footer="Ball_footer")
-    apl.group.edit_group_by_id(group.id, new_group_data)
+    new_group_data = Group(name="Josef", header="vitty_header", footer="cobalt_footer")
+    id = group.id
+    apl.group.edit_group_by_id(id, new_group_data)
     new_groups = db.get_group_info()
     assert len(old_groups) == len(new_groups)
-    #new_groups = map(clean, db.get_group_info())  # -- also can insert here --
-    assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
+    mod_groups = apl.group.get_group_info()
+    def clean(group):
+        return Group(id=group.id, name=group.name.strip(), header=group.header.strip(), footer=group.footer.strip())
+    new_groups = list(map(clean, db.get_group_info()))
+    assert sorted(new_groups, key=Group.id_or_max) == sorted(mod_groups, key=Group.id_or_max)
     if check_ui:
         new_groups = map(clean, db.get_group_info())  # -- also can insert here --
         assert sorted(new_groups, key=Group.id_or_max) == sorted(apl.group.get_group_info(), key=Group.id_or_max)
 
 
-def clean(s):
-    return re.sub("[ ]", "", s)
+def clear(str):
+    return re.sub("[ ]", "", str)
 #def clean(group):
      #   return Group(id=group.id, name=group.name.strip())
 
